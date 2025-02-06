2dd6a01f7851a8d561b037c4abf31c4b0a2faa28
vinod kumar singh <singhvinod40@gmail.com>
2025-02-06 23:01:24 +0530

    Added cards and filter via category

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 import NavBar from "./NavBar";
+import Categories from "./Category";
+import Card from "./Card";
+import food_items from './food';
 
 function Home() {
+
+    let [category, setCategory] = useState(food_items);
+
+    function filterCategoty(selectedCategory) {
+        if (selectedCategory === 'All') {
+            setCategory(food_items);
+        } else {
+            let newList = food_items.filter((item) => item.food_category === selectedCategory);
+            setCategory(newList);
+
+        }
+    }
+
     return (
         <div className="bg-slate-200 w-full min-h-screen">
-            <NavBar/>
+            <NavBar />
+            <div className="w-[100%] flex flex-wrap justify-center items-center gap-5 p-3">
+                {Categories.map((item) => {
+                    return (
+                        // Assuming Categories has a unique 'id' or another unique field, use that
+                        <div
+                            className="w-[140px] h-[140px] bg-white flex flex-col items-start gap-5 p-5 justify-start text-[20px] 
+                            font-semibold text-gray-500 rounded-lg shadow-md hover:bg-green-200 transition-all duration-200"
+                            key={item.id}  onClick={ () => filterCategoty(item.name)} // Use item.id or any unique identifier here
+                        >
+                            {item.icon}
+                            {item.name}
+                        </div>
+                    );
+                }) }
+            </div>
+
+            <div className="  w-full flex flex-wrap gap-6 px-4 pt-4  pb-8 justify-center items-center " >
+                {category.map((item) => (
+                    <Card
+                        key={item.id}
+                        name={item.food_name}
+                        image={item.food_image}
+                        id={item.id}
+                        price={item.price}
+                        type={item.food_type}
+                    />
+                ))}
+            </div>
         </div>
     );
 }
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,17 +4,19 @@ import { FaSearch } from "react-icons/fa";
 import { IoCartOutline } from "react-icons/io5";
 
 function NavBar() {
+
+    let [search,setsearch] = '';
     return (
-        <div className='w-full h-[60px] flex justify-between items-center px-8'>
+        <div className='w-full h-[60px] flex justify-between items-center px-5 md:px-8 '>
 
             <div className='w-[45px] h-[45px] bg-white flex justify-center items-center rounded-md shadow-md'>
                 <IoFastFoodOutline className='w-[30px] h-[30px] text-green-500 ' />
             </div>
 
-            <form className='w-[70%] h-[40px] bg-white flex items-center px-5 gap-5  rounded-md shadow-md '>
+            <form className='w-[45%] h-[40px] bg-white flex items-center px-5 gap-5  rounded-md shadow-md md:w-[70%]'>
 
-                <FaSearch  className='w-[20px] h-[20px] text-green-500'/>
-                <input  className='w-[100%] outline-none text-[20px]' type='text' placeholder='Search items.....' />
+                <FaSearch  onChange={()=> filterCategoty()} className='w-[20px] h-[20px] text-green-500'/>
+                <input  className='w-[100%] outline-none text-[16px] md:text-[20px] ' type='text' placeholder='Search items.....' />
             </form>
 
             <div className='w-[45px] h-[45px] bg-white flex justify-center items-center rounded-md shadow-md relative'>
